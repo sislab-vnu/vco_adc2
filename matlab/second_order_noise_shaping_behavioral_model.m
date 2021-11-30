2739be1b4333cd18b7017b81c937b4962f40c179
duck-manh-tran <ducmanh.m18@gmail.com>
2021-11-30 09:55:29 +0700

    save the matlab model

--- matlab/second_order_noise_shaping_behavioral_model.m
+++ matlab/second_order_noise_shaping_behavioral_model.m
@@ -2,17 +2,17 @@
 close all;
 
 %% ----------------- Overall system ----------------
-trans_time = 0.5e-3;                     % Transient time of simulation
-Fs = 20*10^6;                          % Over sampling frequency of ADC
+trans_time = 4e-3;                     % Transient time of simulation
+Fs = 24*10^6;                          % Over sampling frequency of ADC
 t_s = 0:(1/Fs):(trans_time);           % Discrete time
 div = 40;                              % Time division
 Fs_dis = Fs*div;
 t = 0:1/Fs_dis:trans_time;           % Display continious time range of sumlation
-OSR = 100;                             % Over sampling rate
+OSR = 500;                             % Over sampling rate
 
 %%  -----------------Input signal Block--------------
 f1 = 9*10^3;                        %set up frequency of input signal (Hz)
-V_ip = 0.1*sin(2*pi*f1*t);
+V_ip = 0.25*cos(2*pi*f1*t);
 %V_in =-0.4*sin(2*pi*f1*t)+0.6;
 
 %% --- Init VCO, DCO phase, voltage, K_vco,... -----------
@@ -31,8 +31,8 @@ phase_init = phase_init + 2;    % Phase_init + 2*pi (make all phase value greate
 phase_v1 = phase_init;          % Initate phase_v1 (phases of VCO1)
 phase_d2 = phase_init;          % Initate phase_v1 (phases of DCO2)
 % init counter values
-counter1 = zeros(nop,div+1);
-counter2 = zeros(nop,div+1);
+counter1 = zeros(nop,div+1);    %rpt1 = [];
+counter2 = zeros(nop,div+1);    %rpt2 = [];
 % initate quantizer(qtz) values
 qtz = zeros(nop,1);
 % initate idac
@@ -40,6 +40,7 @@ V_bs2 = 0.4;
 V_dco2 = [];
 K_vco = [7.2 4.8]*10^6;  % [sensitive  central_frequency]
 K_dco = [17.5 2.5]*10^6;  % [sensitive  central_frequency]
+
 %% Second order noise shapping
 for i = 1:length(t_s)-1
     t_i = (i-1)*div+1:i*div+1;      % time between two sample
@@ -51,8 +52,9 @@ for i = 1:length(t_s)-1
     phase_v1(:,t_i) = mod(phase_v1(:,t_i), 2);
     
     % get rise pulse time (rpt) of phase_v1 signal
-    [~, rpt] = find((fix(mod(phase_v1(:,t_i(2:end)), 2)).*~fix(mod(phase_v1(:,t_i(1:(end-1))), 2)))==1);
-    
+    if (i > 1)
+        [~, rpt] = find((fix(mod(phase_v1(:,t_i(1:end)), 2)).*~fix(mod(phase_v1(:,t_i(1:end)-1), 2)))==1);
+    end
     % initate counter1 in new clock cycle
     if(i>1)
         if (qtz(:, i-1)==1)
@@ -62,7 +64,7 @@ for i = 1:length(t_s)-1
         end
     end
     % up down counter hold high value of phase_v1 signal
-    counter1(:,t_i(rpt+1:end)) = 1;
+    counter1(:,t_i(rpt:end)) = 1;
     
     % caculate voltage supply for DCO_2
     V_dco2(t_i) = counter1(:,t_i)*V_bs2;
@@ -71,13 +73,15 @@ for i = 1:length(t_s)-1
     [phase_d2(:,t_i), freq_d2(:,t_i)] = RO_phase_gen(V_dco2(t_i), Fs_dis, K_dco, phase_d2(:, t_i(1)), div);
     
     % get rise pulse time (rpt) of phase_d2 signal
-    [~, rpt] = find((fix(mod(phase_d2(:,t_i(2:end)), 2)).*~fix(mod(phase_d2(:,t_i(1:end-1)), 2)))==1);
-    
+    if (i>1)
+        %[~, rpt] = find((fix(mod(phase_d2(:,t_i(1:end)), 2)).*~fix(mod(phase_d2(:,t_i(1:end)-1), 2)))==1);
+        [~, rpt] = find((fix(mod(phase_d2(:,t_i(2:end)), 2)).*~fix(mod(phase_d2(:,t_i(1:end-1)), 2)))==1);
+    end
     % initate counter2 in new clock cycle
     counter2(:,t_i) = 0;
     
     % up down counter hold high value of phase_d2 signal
-    counter2(:,t_i(rpt+1:end)) = 1;
+    counter2(:,t_i(rpt:end)) = 1;
     
     % phase_d2 quantization (qtz)
     qtz(:, i) = counter2(:,t_i(end));
@@ -85,14 +89,7 @@ end
  
 %% Plot result 
 id_img =1; num_img = 7;         % define figure results
-%figure(1)
-%plot_fft(qtz, Fs, 8192*8)
-% % plot (qtz);
-% figure(2)
-% plot(decimation(qtz, Fs, OSR));
-% figure(3)
-% output = decimation(qtz, Fs, OSR);
-% plot_fft(output, Fs/OSR, 256);
+figure(1)
 subplot(num_img,1,id_img); plot(freq_v1, 'r','LineWidth', 2.2);
 set(gca,'xtick',[0:40:40*OSR]); grid on; id_img=id_img+1;
 xlim([0 40*OSR]); set(gca,'xticklabel',{[]}); title('vco1 freq');
@@ -117,15 +114,27 @@ subplot(num_img,1,id_img); plot(counter2, 'r','LineWidth', 2.2);
 set(gca,'xtick',[0:40:40*OSR]); grid on; id_img=id_img+1;
 xlim([0 40*OSR]); set(gca,'xticklabel',{[]}); title('counter2');
 
-subplot(num_img,1,id_img); stem(qtz, 'r','LineWidth', 2.2);
-set(gca,'xtick',[0:1:OSR]); grid on; id_img=id_img+1;
-xlim([0 OSR]); set(gca,'xticklabel',{[]}); title('quantize value');
+subplot(num_img,1,id_img); plot(counter2_method2, 'r','LineWidth', 2.2);
+set(gca,'xtick',[0:40:40*OSR]); grid on; id_img=id_img+1;
+xlim([0 40*OSR]); set(gca,'xticklabel',{[]}); title('counter2_med2');
+
+%subplot(num_img,1,id_img); stem(qtz, 'r','LineWidth', 2.2);
+%set(gca,'xtick',[0:1:OSR]); grid on; id_img=id_img+1;
+%xlim([0 OSR]); set(gca,'xticklabel',{[]}); title('quantize value');
 
 count2 = sum (qtz(1:OSR))
+% subplot(num_img,1,5);
+% stem(qtz, 'r','LineWidth', 2.2);
+% set(gca,'xtick',[0:1:OSR]);
+% grid on
+% set(gca,'xticklabel',{[]})
 
-
-% % subplot(num_img,1,5);
-% % stem(qtz, 'r','LineWidth', 2.2);
-% % set(gca,'xtick',[0:1:OSR]);
-% % grid on
-% % set(gca,'xticklabel',{[]})
+figure(2);
+plot_fft(qtz, Fs, 8192*8)
+xlim([0 24000]);
+% % plot (qtz);
+% figure(3)
+% plot(decimation(qtz, Fs, OSR));
+% figure(3)
+% output = decimation(qtz, Fs, OSR);
+% plot_fft(output, Fs/OSR, 256);
