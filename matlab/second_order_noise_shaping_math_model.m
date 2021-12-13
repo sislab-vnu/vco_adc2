5d5a8c3c4d07ec0c2a636dd7a9c3c6494ef9cc70
duck-manh-tran <ducmanh.m18@gmail.com>
2021-12-13 23:32:16 +0700

    up date math model for second order ADC

--- matlab/second_order_noise_shaping_math_model.m
+++ matlab/second_order_noise_shaping_math_model.m
@@ -1,7 +1,7 @@
 % matlab mathematic model of second order noise shapping
-OSR = 250;
+OSR = 500;
 OBG = 2.5;
-fs = 12*10^6;
+fs = 24*10^6;
 % ntf = synthesizeNTF(3, OSR, 0, OBG, 0);
 % ntf = ()
 % L1 = 1/ntf -1;
@@ -10,18 +10,18 @@ fs = 12*10^6;
 % x2 = impulse(c2d(tf([1], [1 0 0]), 1), 10);
 % 
 % K = [x1 x2]\l;
-Kv1 = 0.36;  f0 = 0.24;
-Kv2 = 0.36;  f0 = 0.2;
+Kv1 = 0.3;  f0_1 = 0.2;
+Kv2 = 0.3;  f0_2 = 0.25;
 
 Ac = [0 0; Kv2 0];
-Bc = [Kv1 f0 -1; 0 f0 -1;];
+Bc = [Kv1 f0_1 -1; 0 f0_2 -1;];
 Cc = [0 1];
 Dc = [0 0 0];
 
 sys_ct = ss(Ac, Bc, Cc, Dc);
 sys_dt = c2d(sys_ct, 1);
 
-u = 0.42*sin(2*pi*0.2/OSR*(0:1:2^15));
+u = 0.32*sin(2*pi*0.2/OSR*(0:1:2^15));
 K_offset = ones(1, length(u));
 u_ = [u ; K_offset];
 Ad = sys_dt.A;
@@ -30,7 +30,7 @@ Cd = sys_dt.C;
 Dd = sys_dt.D;
 
 ABCD = [Ad Bd; Cd Dd];
-[v,xn,xmax,y] = simulateDSM(u_, ABCD, 6, zeros(2,1));
+[v,xn,xmax,y] = simulateDSM(u_, ABCD, 2, zeros(2,1));
 % plot(v);
 
 
